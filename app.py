bae847fb8929e0ad70c9f40f69546e0f31e51b1c
EliteMinerZMC <joshua.middleton@live.co.uk>
2017-08-15 23:07:07 +0100

    push - threading

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import colorsys
-
+import cv2
+import threading
 
 app = Flask(__name__)
 
 object_colour = ""
-
+thread1 = threading.Thread()
+capture_bool = True
 @app.route('/')
 def index():
     return render_template('home.jinja2', name="meow")
@@ -18,8 +20,27 @@ def parse_request():
     object_colour = colour.lstrip('#')
     numericalcolour=tuple(int(object_colour[i:i+2], 16) for i in (0, 2 ,4))
     hsv=colorsys.rgb_to_hsv(object_colour[:2],object_colour[2:4],object_colour[4:6])
-    print_colour = hsv
-    # need posted data here
+
+@app.route('/run', methods=['GET','POST'])
+def start_thread():
+    #return threading.Thread(target=video_capture())
+    return video_capture()
+
+
+@app.route('/stop', methods=['GET','POST'])
+def thread_stop():
+    capture_bool = False
+    return index()
+
+
+def video_capture():
+    camera = cv2.VideoCapture(0)
+    while capture_bool:
+        if True:
+            pass
+        else:
+            (grabbed, frame) = camera.read()
+
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -29,6 +50,6 @@ def hello(name=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',threaded=True)
 
 
